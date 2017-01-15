c9d6ea569295a4e112567900adfe4e6ee662421c
Feliciano Santana <fsfelicianosantana@gmail.com>
2017-01-15 13:36:23 -0300

    Add network component

---
+++ test/network-component.test.js
@@ -0,0 +1,15 @@
+import { NetworkComponent } from "../lib";
+import { expect } from "chai";
+
+describe("NetworkComponent", () => {
+    it("should throw error if registering invalid messageHandlers", () => {
+        const component = new NetworkComponent();
+        expect(component.registerMessageHandlers.bind(this, {abc: "abc"})).to.throw(Error);
+    });
+
+    it("should allow message retrieval", () => {
+        const component = new NetworkComponent();
+        component.registerMessageHandlers({1: () => {}});
+        expect(component.getMessageHandler(1)).to.be.a('function');
+    });
+});
